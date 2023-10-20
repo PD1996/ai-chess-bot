343b9fcd4d1e07665f291882caee8f7f2ce40ca4
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-10-19 20:49:54 -0700

    Add styling and drag controls to chessboard

--- frontend/src/Chessboard.js
+++ frontend/src/Chessboard.js
@@ -1,42 +1,99 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Chess } from "chess.js";
+import whiteKing from "./assets/white_king.png";
+import whiteQueen from "./assets/white_queen.png";
+import whiteRook from "./assets/white_rook.png";
+import whiteBishop from "./assets/white_bishop.png";
+import whiteKnight from "./assets/white_knight.png";
+import whitePawn from "./assets/white_pawn.png";
+import blackKing from "./assets/black_king.png";
+import blackQueen from "./assets/black_queen.png";
+import blackRook from "./assets/black_rook.png";
+import blackBishop from "./assets/black_bishop.png";
+import blackKnight from "./assets/black_knight.png";
+import blackPawn from "./assets/black_pawn.png";
+import "./Chessboard.css";
+
+const imageMapping = {
+  wK: whiteKing,
+  wQ: whiteQueen,
+  wR: whiteRook,
+  wB: whiteBishop,
+  wN: whiteKnight,
+  wP: whitePawn,
+  bK: blackKing,
+  bQ: blackQueen,
+  bR: blackRook,
+  bB: blackBishop,
+  bN: blackKnight,
+  bP: blackPawn,
+};
 
 const Chessboard = () => {
-  const [board, setBoard] = useState(Array(8).fill(Array(8).fill(".")));
-  const [move, setMove] = useState("");
+  const [board, setBoard] = useState(new Chess());
+  const [fen, setFen] = useState("start");
+  const [dragging, setDragging] = useState(null);
 
-  const handleMove = async () => {
+  const loadBoard = async () => {
     try {
       const response = await axios.post("http://localhost:5001/move", {
-        move,
+        fen: board.fen(),
       });
-      console.log("Response from server:", response.data);
-
-      const boardString = response.data.board;
-      const boardArray = boardString.split("\n").map((row) => row.split(" "));
-      setBoard(boardArray);
+      const newFen = response.data.board;
+      setFen(newFen);
+      board.load(newFen);
     } catch (error) {
-      console.log("Error sending move:", error);
+      console.log("Error loading board:", error);
     }
+  };
+
+  const handleDrop = (to) => {
+    const move = board.move({ from: dragging, to: to, promotion: "q" });
+    if (move === null) return;
+    loadBoard();
+  };
+
+  useEffect(() => {
+    loadBoard();
+  }, []);
+
+  const renderBoard = () => {
+    const squares = [];
+    for (let i = 7; i >= 0; i--) {
+      for (let j = 0; j <= 7; j++) {
+        const square = String.fromCharCode(97 + j) + (i + 1);
+        const piece = board.get(square);
+        const color = (i + j) % 2 === 0 ? "black" : "white";
+        const pieceCode = piece
+          ? `${piece.color}${piece.type.toUpperCase()}`
+          : "";
+        squares.push(
+          <div
+            key={square}
+            className={`square ${color}`}
+            onDragOver={(e) => e.preventDefault()}
+            onDrop={() => handleDrop(square)}
+          >
+            {piece && (
+              <span
+                draggable
+                onDragStart={() => setDragging(square)}
+                className="piece"
+                style={{ backgroundImage: `url(${imageMapping[pieceCode]})` }}
+              ></span>
+            )}
+          </div>
+        );
+      }
+    }
+    return squares;
   };
 
   return (
     <div>
-      <div>
-        {board.map((row, i) => (
-          <div key={i}>
-            {row.map((cell, j) => (
-              <span key={j}>{cell}</span>
-            ))}
-          </div>
-        ))}
-      </div>
-      <input
-        type="text"
-        value={move}
-        onChange={(e) => setMove(e.target.value)}
-      />
-      <button onClick={handleMove}>Make Move</button>
+      <div className="chessboard">{renderBoard()}</div>
+      <button onClick={loadBoard}>Refresh Board</button>
     </div>
   );
 };
